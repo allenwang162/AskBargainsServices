903732a5216405f9e407df23af1c44d8d94ea94c
Allen Wang <allen.y.wang@gmail.com>
2012-03-28 15:27:22 -0400

    Project created

---
+++ AskBargainsServices/IDataFeedService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace AskBargainsServices
+{
+    // Service Contract for Loading DataFeed
+    [ServiceContract]
+    public interface IDataFeedService
+    {
+
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        ItemInfo GetDataUsingDataContract(ItemInfo composite);
+
+        // TODO: Add your service operations here
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ItemInfo
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
