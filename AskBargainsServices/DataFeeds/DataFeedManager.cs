3bf9c1b1d786f7c333f90616c921c53a02bf924e
Allen Wang <allen.y.wang@gmail.com>
2012-03-29 15:58:26 -0400

    DataFeedManager added

---
+++ AskBargainsServices/DataFeeds/DataFeedManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AskBargainsServices.DataContracts;
+using System.IO;
+
+namespace AskBargainsServices.DataFeeds
+{
+    public class DataFeedManager
+    {
+        public static IList<DataInfo> LoadAllDataFeeds()
+        {
+            var path = Environment.CurrentDirectory;
+            var info = Directory.EnumerateFiles("","*xml",SearchOption.TopDirectoryOnly);
+
+            return null;
+        }
+    }
+}
